beaf86e1847714970b49deacc82d180435378277
JATIN CHULET <2024EB02230@online.bits-pilani.ac.in>
2025-10-25 15:08:44 +0530

    Update ChatBot.jsx

--- client/src/pages/ChatBot.jsx
+++ client/src/pages/ChatBot.jsx
@@ -29,7 +29,7 @@ const ChatBot = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("http://localhost:8080/api/v1/openai/chatbot", { text });
+      const { data } = await axios.post("https://chatgpt-clone-s2ok.onrender.com", { text });
       console.log(data);
       setResponse(data);
     } catch (err) {
